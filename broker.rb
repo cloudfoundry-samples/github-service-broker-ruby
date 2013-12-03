c67be1fe688eb4e30c3f089918dc1ab6c5f1546f
Alex Stupakov and Karen Wang <pair+astupakov+khwang@pivotallabs.com>
2013-12-03 15:39:23 -0800

    implement a sinatra app and the catalog endpoint

---
+++ broker.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'json'
+
+get "/v2/catalog" do
+  content_type :json
+
+  description = {
+    "services" => [{
+        "id" => "echo-service",
+        "name"=> "Echo Service",
+        "description"=> "A service that echoes back the body of each web request it receives",
+        "plans"=> [{
+          "id"=> "free-plan",
+          "name"=> "free",
+          "description"=> "Echoing does not cost money."
+        },{
+          "id"=> "expensive-plan",
+          "name"=> "expensive",
+          "description"=> "Echoing costs lots of money."
+        }]
+      }]
+  }
+
+  description.to_json
+end
