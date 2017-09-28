3b758327ed81fc21896b18a45efb3ef46a54aae5
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-09-28 11:02:03 +0200

    Catch Faraday::ConnectionFailed, too

--- service_broker/test/test_helper.rb
+++ service_broker/test/test_helper.rb
@@ -1,15 +1,11 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 ENV['RACK_ENV'] = 'test'
+
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'rack/test'
 require 'mocha/setup'
 require 'webmock/minitest'
 require 'pry'
-
-WebMock.disable_net_connect!(allow: 'codeclimate.com')
 
 SETTINGS_FILENAME = "test/config/settings.yml"
 
